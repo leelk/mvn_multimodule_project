20d7123789353880aade958e1b2bde9489114a4c
Leel Karunarathne <leel@rumex.lk>
2021-01-11 11:05:13 +0530

    initial

---
+++ src/main/java/lk/rumex/hospitalmanagmentsystem/HospitalManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.rumex.hospitalmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/rumex/hospitalmanagmentsystem/HospitalManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.rumex.hospitalmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
